f1924bc0cf001bac31f73b2ca965066ab54efea7
nbourdi <nataliabourdh@gmail.com>
2023-01-28 20:21:17 +0200

    Modal sheet fixes & Auto-silence options.

--- lib/SettingsScreen.dart
+++ lib/SettingsScreen.dart
@@ -3,15 +3,18 @@ import 'package:arise/NameField.dart';
 import 'package:flutter/material.dart';
 
 
+
 class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return _SettingsScreen();
-      }
-    }
-    
+  }
+}
+
 class _SettingsScreen extends State<SettingsScreen>{
-  // var _currentSliderValue;
+  static const List<String> silence_min = ['3 minutes', '5 minutes', '8 minutes'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,48 +28,111 @@ class _SettingsScreen extends State<SettingsScreen>{
       ),
       body:
       ListView(
-        padding: EdgeInsets.all(22),
+        padding: const EdgeInsets.all(22),
         children: <Widget>[
 
           ListTile(
-            title: Text('Profile', style: TextStyle(fontFamily: 'Inter')),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              showModalBottomSheet(
+              title: const Text('Profile', style: TextStyle(fontFamily: 'Inter')),
+              trailing: const Icon(Icons.arrow_right),
+              onTap: () {
+                showModalBottomSheet(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(25.0))),
 
-                  shape: const RoundedRectangleBorder(
-
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(25),
-                          topRight: Radius.circular(25))),
-                  context: context,
-                  builder: (BuildContext context) {
-                    return SizedBox(
-                      height: 300,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Profile',
-                              style:
-                              Theme.of(context).textTheme.headlineSmall,
+                    context: context,
+                    isScrollControlled: true,
+                    builder: (context) => Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          ),
+                          SizedBox(
+                            height: 280,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'Profile',
+                                    style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                                  ),
+                                  const LocationField(),
+                                  const NameField(),
+                                ],
+                              ),
                             ),
-                            const LocationField(),
-                            const NameField(),
-                          ],
-                        ),
+                          ),
+
+
+
+                          SizedBox(height: 10),
+                        ],
                       ),
-                    );
-                  });
-            }
+                    ));
+                // showModalBottomSheet(
+                //
+                //     shape: const RoundedRectangleBorder(
+                //         borderRadius: BorderRadius.only(
+                //             topLeft: Radius.circular(25),
+                //             topRight: Radius.circular(25))),
+                //     context: context,
+                //     builder: (context) => Padding(
+                //       padding: MediaQuery.of(context).viewInsets,
+                //
+                //       child: SizedBox(
+                //       height: 300,
+                //         child: Center(
+                //         child: Column(
+                //           mainAxisAlignment: MainAxisAlignment.center,
+                //           children: <Widget>[
+                //             Text(
+                //               'Profile',
+                //               style:
+                //               Theme.of(context).textTheme.headlineSmall,
+                //             ),
+                //             const LocationField(),
+                //             const NameField(),
+                //           ],
+                //         ),
+                //       ),
+                //     )
+                //
+                //
+                //       // return SizedBox(
+                //       //   height: 300,
+                //       //
+                //       //   child: Center(
+                //       //     child: Column(
+                //       //       mainAxisAlignment: MainAxisAlignment.center,
+                //       //       children: <Widget>[
+                //       //         Text(
+                //       //           'Profile',
+                //       //           style:
+                //       //           Theme.of(context).textTheme.headlineSmall,
+                //       //         ),
+                //       //         const LocationField(),
+                //       //         const NameField(),
+                //       //       ],
+                //       //     ),
+                //       //   ),
+                //       //
+                //       // );}
+                //
+                //     ));
+              }
           ),
-          
-          Divider(),
+
+          const Divider(),
           ListTile(
-            title: Text('Volume', style: TextStyle(fontFamily: 'Inter')),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
+              title: Text('Volume', style: TextStyle(fontFamily: 'Inter')),
+              trailing: Icon(Icons.arrow_right),
+              onTap: () {
                 showModalBottomSheet(
                     shape: const RoundedRectangleBorder(
                         borderRadius: BorderRadius.only(
@@ -92,50 +158,93 @@ class _SettingsScreen extends State<SettingsScreen>{
               }
           ),
           const Divider(),
-          const ListTile(
-            title: Text('Auto-Silence'),
-            trailing: Icon(Icons.arrow_right),
-          ),
+          ListTile(
+              title: const Text("Auto-Silence"),
+              trailing: const Icon(Icons.arrow_right),
+              onTap: () {
+                showModalBottomSheet(
+
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25))),
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SizedBox(
+                          height: 200,
+                          child: Center(
+                              child: ListView(
+                                padding: const EdgeInsets.only(
+                                    left: 15, right: 15, top: 16),
+                                children: [
+                                  ListTile(
+                                    title: const Text("3 minutes"),
+                                    onTap: () {
+                                      setState(() {
+                                        // alarmInfo.daysActive = 'Once';
+                                        Navigator.pop(context);
+                                      });
+                                    },
+                                  ),
+                                  ListTile(
+                                    title: const Text("5 minutes"),
+                                    onTap: () {
+                                      setState(() {
+                                        // alarmInfo.daysActive = Days.getFormatted(
+                                        //      [0, 1, 2, 3, 4, 5, 6],
+                                        //      compact: true);
+                                        Navigator.pop(context);
+                                      });
+                                    },
+                                  ),
+                                  ListTile(
+                                    title: const Text("8 minutes"),
+                                    onTap: () {},
+                                  )
+                                ],
+                              )));
+                    });
+              }),
           const Divider(),
           ListTile(
-            title: const Text('Contact Us'),
-            trailing: const Icon(Icons.arrow_right),
-            onTap: () {
-              AlertDialog alert = const AlertDialog(
-                title: Text('Contact Us'),
-                content: Text('If you have any questions or problems while using ARISE! contact us through the following page: \nhttps://github.com/AndrewP77/ARISE',
-                ),
-              );
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return alert;
-                },
-              );
-            }
+              title: const Text('Contact Us'),
+              trailing: const Icon(Icons.arrow_right),
+              onTap: () {
+                AlertDialog alert = const AlertDialog(
+                  title: Text('Contact Us'),
+                  content: Text('Ask your mom for our contact',
+                  ),
+                );
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return alert;
+                  },
+                );
+              }
           ),
           Divider(),
           ListTile(
-            title: Text('About'),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              AlertDialog alert = AlertDialog(
-                title: Text('About'),
-                content: Text('ARISE! version 1.1. \nThe latest version is installed. \nMIT license. \n'),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'OK'),
-                    child: const Text('OK'),
-                  ),
-                ],
-              );
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return alert;
-                },
-              );
-            }
+              title: Text('About'),
+              trailing: Icon(Icons.arrow_right),
+              onTap: () {
+                AlertDialog alert = AlertDialog(
+                  title: Text('About'),
+                  content: Text('ARISE! version 1.1. \nThe latest version is installed. \nMIT license. \n'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'OK'),
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return alert;
+                  },
+                );
+              }
           ),
         ],
       ),
@@ -143,7 +252,9 @@ class _SettingsScreen extends State<SettingsScreen>{
   }
 }
 
-  
+
+
+
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
@@ -156,7 +267,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   static int _currentSliderValue = 40;
   @override
   Widget build(BuildContext context) {
-    
+
     return Center(
       child: Column(
         children: <Widget>[
